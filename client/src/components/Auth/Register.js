484c4da3b4a062b6b02736624861281559e8bedd
vic30004 <victor_993@msn.com>
2019-12-13 13:51:41 -0800

    we are able to register users via firebase

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
 import {Grid, Form,Segment,Button,Header,Message,Icon} from 'semantic-ui-react'
 import {Link} from "react-router-dom"
+import firebase from '../../firebase'
 export default class Register extends Component {
-    state ={}
+    state ={
+        username:'',
+        email:'',
+        password:'',
+        passwordConfirmation:''
+    }
 
-    handleChange = () =>{}
+    handleChange = (e) =>{
+        this.setState({[e.target.name]:e.target.value})
+    }
+
+    handleSubmit = (e)=>{
+        e.preventDefault();
+        firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email,this.state.password)
+        .then(createdUser=>{
+            console.log(createdUser);
+        })
+        .catch(err =>{
+            console.error(err)
+        })
+
+    }
     render() {
+        const {username,email,passwordConfirmation,password}=this.state
         return (
             <div>
                <Grid textAlign='center' verticalAlign='middle' className="app">
@@ -15,12 +38,12 @@ export default class Register extends Component {
                             Register For codeHub Chat! 
                         </Header>
 
-                    <Form size="large">
+                    <Form onSubmit={this.handleSubmit} size="large">
                         <Segment stacked>
-                            <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" onChange={this.handleChange} type="text"/>
-                            <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="Email" onChange={this.handleChange} type="email"/>
-                            <Form.Input fluid name="password" icon="key" iconPosition="left" placeholder="Password" onChange={this.handleChange} type="password"/>
-                            <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" placeholder="Confirm Password" onChange={this.handleChange} type="password"/>
+                            <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" onChange={this.handleChange} value={username} type="text"/>
+                            <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="Email" onChange={this.handleChange} value={email} type="email"/>
+                            <Form.Input fluid name="password" icon="key" iconPosition="left" placeholder="Password" onChange={this.handleChange} value={password} type="password"/>
+                            <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" placeholder="Confirm Password" value={passwordConfirmation} onChange={this.handleChange} type="password"/>
                             <Button color="yellow" fluid size="large">Submit</Button>
                         </Segment>
                     </Form>
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-
+import './components/App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
