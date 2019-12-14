f14f6c30800e82cf4fde223a138496cb992733ee
vic30004 <victor_993@msn.com>
2019-12-13 18:34:16 -0800

    set up redux store and set user function

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './components/App.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './components/App.css';
+import { BrowserRouter as Router, Switch, Route,withRouter } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
+import firebase from './firebase';
+import {createStore} from 'redux';
+import {Provider,connect} from 'react-redux'
+import {composeWithDevTools} from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import {setUser} from './actions'
+const store = createStore (rootReducer, composeWithDevTools());
 
-const Root = () => (
+class Root extends React.Component {
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user=>{
+      if(user){
+        this.props.setUser(user)
+        this.props.history.push("/");
+      }
+    })
+  }
+  render() {
+    return (
+        <Switch>
+          <Route exact path='/' component={App} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+        </Switch>
+    );
+  }
+}
+
+const RootWithAuth = withRouter(connect(null, {setUser})(Root))
+
+ReactDOM.render(
+  <Provider store={store}>
   <Router>
-    <Switch>
-      <Route exact path='/' component={App} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/register' component={Register} />
-    </Switch>
-  </Router>
-);
-
-ReactDOM.render(<Root />, document.getElementById('root'));
+  <RootWithAuth />
+   </Router>
+   </Provider>
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
