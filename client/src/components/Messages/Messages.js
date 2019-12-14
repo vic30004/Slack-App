4a373fc6e7c8a6fda40c0bb413bfaf478e93cfea
vic30004 <victor_993@msn.com>
2019-12-14 10:36:44 -0800

    created the collection for the messages and added it to the app

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,23 +1,31 @@
-import React, { Component,Fragment } from 'react'
-import {Segment,Comment} from 'semantic-ui-react'
-import MessagessHeader from './MessagesHeader'
-import MessageForm from './MessageForm'
+import React, { Component, Fragment } from 'react';
+import { Segment, Comment } from 'semantic-ui-react';
+import MessagessHeader from './MessagesHeader';
+import MessageForm from './MessageForm';
+import firebase from '../../firebase';
 export class Messages extends Component {
-    render() {
-        return (
-           <Fragment>
-            <MessagessHeader/>
-           <Segment>
-           
-            <Comment.Group className="messages">
-            
-            
-            </Comment.Group>
-           </Segment>
-           <MessageForm/>
-           </Fragment>
-        )
-    }
+  state = {
+    messagesRef: firebase.database().ref('messages'),
+    channel: this.props.currentChannel,
+    user: this.props.currentUser
+  };
+  render() {
+
+    const { messagesRef,channel } = this.state;
+    const {currentUser} = this.props
+    return (
+      <Fragment>
+        <MessagessHeader />
+        <Segment>
+          <Comment.Group className='messages'></Comment.Group>
+        </Segment>
+        <MessageForm 
+        currentChannel={channel}
+        messagesRef={messagesRef}
+        currentUser={currentUser} />
+      </Fragment>
+    );
+  }
 }
 
-export default Messages
+export default Messages;
