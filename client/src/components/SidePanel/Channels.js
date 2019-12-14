2ca518995bf10028ba43216ebb069bd938026813
vic30004 <victor_993@msn.com>
2019-12-13 22:01:43 -0800

    app now displays which channel is currently active

--- client/src/components/SidePanel/Channels.js
+++ client/src/components/SidePanel/Channels.js
@@ -6,12 +6,14 @@ import { setCurrentChannel } from '../../actions';
 
 export class Channels extends Component {
   state = {
+    activeChannel:'',
     user: this.props.currentUser,
     channels: [],
     channelName: '',
     channelDetails: '',
     channelsRef: firebase.database().ref('channels'),
-    modal: false
+    modal: false,
+    firstLoad: true
   };
 
   componentDidMount() {
@@ -22,9 +24,20 @@ export class Channels extends Component {
     let loadedChannels = [];
     this.state.channelsRef.on('child_added', snap => {
       loadedChannels.push(snap.val());
-      this.setState({ channels: loadedChannels });
+      this.setState({ channels: loadedChannels }, ()=>this.setFirstChannel());
     });
   };
+
+  setFirstChannel = ()=>{
+      const firstChannel = this.state.channels[0]
+      if(this.state.firstLoad && this.state.channels.length>0 ){
+          this.props.setCurrentChannel(firstChannel)
+          this.setActiveChannel(firstChannel)
+
+          
+      }
+      this.setState({firstLoad:false})
+  }
 
   handleSubmit = e => {
     e.preventDefault();
@@ -39,6 +52,7 @@ export class Channels extends Component {
     const key = channelsRef.push().key;
 
     const newChannel = {
+      
       id: key,
       name: channelName,
       details: channelDetails,
@@ -62,8 +76,13 @@ export class Channels extends Component {
   };
 
   changeChannel = channel => {
+      this.setActiveChannel(channel)
     this.props.setCurrentChannel(channel);
   };
+
+  setActiveChannel = channel =>{
+      this.setState({activeChannel:channel.id})
+  }
 
   displayChannels = channels =>
     channels.length > 0 &&
@@ -73,6 +92,7 @@ export class Channels extends Component {
         onClick={() => this.changeChannel(channel)}
         name={channel.name}
         style={{ opacity: 0.7 }}
+        active= {channel.id===this.state.activeChannel}
       >
         # {channel.name}
       </Menu.Item>
